28c933df90435a6da9c4fd43f178f855ef8e4080
Cgotts <cobi.gottschalk@gmail.com>
2022-11-20 10:20:27 -0800

    Added comments

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+let player = {
+  name: "CGotts1",
+  chips: 145,
+  sayHello: function(){
+    console.log("Yo What Up?!")                   //This is a method
+  }
+}
+player.sayHello()                             //Calls the method or the function if you will
+
 let cards = [];
 let sum = 0;
-let hasBlackJack = false;
-let isAlive = false;
+let hasBlackJack = false;                      
+let isAlive = false;                        // false because does not have bj at beginning
 let message = " ";
 let startBtn = document.querySelector("#start-btn");
 let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 let newBtn = document.querySelector("#newcard-btn");
-
-let player = {
-    name: "CGotts1",
-    chips: 145
-}
 
 
 let playerEl = document.querySelector("#player-el")
@@ -22,11 +26,11 @@ console.log(cards)
 
 
 function getRandomCard(){
-    let chooseRandom = Math.floor(Math.random() * 13) + 1
-    if(chooseRandom === 1){
-        return 11
-    } else if(chooseRandom === 11 || chooseRandom === 12 || chooseRandom === 13){
-        return 10
+  let chooseRandom = Math.floor(Math.random() * 13) + 1               // would be 0-13 without the plus 1
+  if(chooseRandom === 1){
+    return 11
+  } else if(chooseRandom === 11 || chooseRandom === 12 || chooseRandom === 13){
+    return 10
     }else {
         return chooseRandom
     }
@@ -74,8 +78,8 @@ function newCard() {
     if (isAlive === true && hasBlackJack === false){
         let card = getRandomCard();
         sum += card;
-        cards.push(card);
-        renderGame();
+        cards.push(card);                              //pushes that new card into the cards array
+        renderGame();                                 // gets another card
         console.log(card);
         console.log(sum);
     } else {
